ce87fb7ab464ab12d1df38e21c46a9dc929efe28
github username <github email address>
2019-11-06 16:32:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+
+const testArr = ["Tom", "Takeshi", "David", "Mikawa", "Yan"]
+
+const returnFirstTwoDrivers = (arr) => {
+    return arr.slice(0,2)
+};
+
+const returnLastTwoDrivers = (arr) =>{
+    return arr.slice(-2)
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (num) =>{
+    return (fare) => { return fare * num
+    }
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (arr, selectingDrivers) =>{
+    return selectingDrivers(arr)
+
+}
